39aefab88e56791bddec0ee91daae7b49c8fb4fb
joannules <joanfelip_nls@hotmail.com>
2016-12-18 18:23:06 +0100

    asd

--- firework.js
+++ firework.js
@@ -1,15 +1,35 @@
-
-
 function Firework() {
 
-  this.particle = new Particle(random(windowWidth),random(windowHeight));
+    this.particle = new Particle(random(windowWidth), random(windowHeight));
+    this.exploded = false;
+    this.fragments = [];
 
-  this.update = function () {
-    this.particle.applyForce(gravity);
-    this.particle.update();
-  }
+    this.update = function() {
+        if (!this.exploded) {
+            this.particle.applyForce(gravity);
+            this.particle.update();
 
-  this.show = function() {
-        this.particle.show();
-  }
+            if (this.particle.vel.y >= 0) {
+                this.exploded = true;
+                this.explode();
+            }
+
+        }
+    }
+
+    this.explode() {
+        for (var i = 0; i < 100; i++) {
+            var fragment = new Particle(this.pos.x, this.pos.y);
+            this.fragments.push(fragment);
+        }
+    }
+
+    this.show = function() {
+        if (!this.exploded) {
+            this.particle.show();
+        }
+        for (var i = 0; i < 100; i++) {
+            this.fragments[i].show;
+        }
+    }
 }
