82a9a4d49a905a89cb1ff0e8fb85ae1a58d7f23b
joannules <joanfelip_nls@hotmail.com>
2016-12-18 17:49:12 +0100

    asd

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+var firework;
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
-
+  stroke(255);
+  strokeWeight(4);
+  firework = new Particle(windowWidth/2,windowHeight/2);
 }
 
 function draw() {
   background(50);
-
+  firework.update();
+  firework.show();
 }
