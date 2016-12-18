084f9eee63f790a4c82ac3af1d8b97a008e1a0d3
joannules <joanfelip_nls@hotmail.com>
2016-12-18 18:00:33 +0100

    a

--- particle.js
+++ particle.js
@@ -1,20 +1,19 @@
+function Particle(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, -10);
+    this.acc = createVector(0, 0);
 
-function Particle (x,y) {
-  this.pos = createVector(x,y);
-  this.vel = createVector(0,-4);
-  this.acc = createVector(0,0);
+    this.applyForce = function(force) {
+        this.acc.add(force);
+    }
 
-  this.applyForce = function(force){
-    acc.add(force);
-  }
+    this.update = function() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
 
-  this.update = function(){
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  this.show = function(){
-    point(this.pos.x,this.pos.y);
-  }
+    this.show = function() {
+        point(this.pos.x, this.pos.y);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 var firework;
+var gravity;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  stroke(255);
-  strokeWeight(4);
-  firework = new Particle(random(windowWidth),random(windowHeight));
+    createCanvas(windowWidth, windowHeight);
+    stroke(255);
+    strokeWeight(4);
+
+    gravity = createVector(0, 0.2);
+    firework = new Particle(random(windowWidth), random(windowHeight));
 }
 
 function draw() {
-  background(50);
-  firework.update();
-  firework.show();
+    background(50);
+    firework.applyForce(gravity);
+    firework.update();
+    firework.show();
 }
